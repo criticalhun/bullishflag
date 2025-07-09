f63cc1cdc0a8970d5fabcf0ed149ba34d0159ff5
criticalhun <criticalhun@proton.me>
2025-07-09 16:27:36 +0200

    build: ignore TypeScript errors in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-// next.config.ts
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! FIGYELEM !!
-    // Ezzel a beállítással a production build sikeresen lefut,
+    // Ezzel a beállítással a build sikeresen lefut,
     // még akkor is, ha a projektben típus-hibák vannak.
-    // !! FIGYELEM !!
     ignoreBuildErrors: true,
   },
 };

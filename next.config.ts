633702015de779be1e2e59a627fb664b6a5c7a52
criticalhun <criticalhun@proton.me>
2025-07-13 19:45:28 +0200

    build: rename next.config.ts to next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Ezzel a beállítással a build sikeresen lefut,
+    // !! FIGYELEM !!
+    // Ezzel a beállítással a production build sikeresen lefut,
     // még akkor is, ha a projektben típus-hibák vannak.
     ignoreBuildErrors: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
